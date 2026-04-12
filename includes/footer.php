9d3e5f79acf81ce1d5ce0516ac0b001efe8588fe
mxkoto <mxkotokatsura@gmail.com>
2026-04-12 16:46:38 +0300

    make footer

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <h1>Контакты</h1>
+        <p>г. Казань, ул. Хз какая</p>
+        <p>+79123456789</p>
+        <p>office@gmail.com</p>
+    </div>
+
+    <script src="../assets/js/main.js"></script>
+</footer>
